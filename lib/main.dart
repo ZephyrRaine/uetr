8e8b4e531a6c37bb06b89bc2e1c4ca2ce9486a2a
Colin <zephyr.raine@gmail.comp>
2021-05-28 14:22:55 +0200

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,7 @@ String getRandomString(int length) {
       length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 }
 
-class Person
-{
+class Person {
   final String name;
   final num money;
   final String location;
@@ -45,107 +44,105 @@ class FoodCard extends StatelessWidget {
     final ButtonStyle textButtonStyle = TextButton.styleFrom(
         primary: Colors.amber.shade500,
         textStyle: descriptionStyle.copyWith(fontSize: 25));
-    return  Container(
+    return Container(
         height: 400,
         padding: EdgeInsets.all(10.0),
         child: Card(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 184.0,
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: Image.network(
-                          'https://picsum.photos/200/300?${getRandomString(10)}',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 16.0,
-                        left: 16.0,
-                        right: 16.0,
-                        child: FittedBox(
-                          fit: BoxFit.scaleDown,
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            myPerson.name,
-                            style: titleStyle,
-                          ),
-                        ),
-                      ),
-                    ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 184.0,
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Image.network(
+                      'https://picsum.photos/200/300?${getRandomString(10)}',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
+                  Positioned(
+                    bottom: 16.0,
+                    left: 16.0,
+                    right: 16.0,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        myPerson.name,
+                        style: titleStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 // Description and share/explore buttons.
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-                  child: DefaultTextStyle(
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    style: descriptionStyle,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-// three line description
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: ListTile(
-                            title: Text(myPerson.location,
-                                style: descriptionStyle.copyWith(
-                                    color: Colors.black54)),
-                            leading: Icon(Icons.location_on_outlined),
-                          ),
-                        ),
-                        Center(
-                            child: Text(
-                                "\$${currencyFormat.format(myPerson.money)}",
-                                style: theme.textTheme.headline4
-                                    .copyWith(color: Colors.black))),
-                      ],
-                    ),
-                  ),
-                ),
-
-// share, explore buttons
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+              child: DefaultTextStyle(
+                softWrap: false,
+                overflow: TextOverflow.ellipsis,
+                style: descriptionStyle,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextButton(
-                      style: textButtonStyle.copyWith(),
-                      onPressed: () {
-                        print('pressed');
-                      },
-                      child: Text('DETAILS', semanticsLabel: 'Eat '),
+// three line description
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: ListTile(
+                        title: Text(myPerson.location,
+                            style: descriptionStyle.copyWith(
+                                color: Colors.black54)),
+                        leading: Icon(Icons.location_on_outlined),
+                      ),
                     ),
-                    TextButton(
-                      style: textButtonStyle,
-                      onPressed: () {
-                        print('pressed');
-                      },
-                      child: Text('EAT', semanticsLabel: 'Explore  '),
-                    ),
+                    Center(
+                        child: Text(
+                            "\$${currencyFormat.format(myPerson.money)}",
+                            style: theme.textTheme.headline4
+                                .copyWith(color: Colors.black))),
                   ],
                 ),
+              ),
+            ),
+
+// share, explore buttons
+            ButtonBar(
+              alignment: MainAxisAlignment.start,
+              children: [
+                TextButton(
+                  style: textButtonStyle.copyWith(),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => PersonDetail(myPerson)));
+                  },
+                  child: Text('DETAILS', semanticsLabel: 'Eat '),
+                ),
+                TextButton(
+                  style: textButtonStyle,
+                  onPressed: () {
+                    print('pressed');
+                  },
+                  child: Text('EAT', semanticsLabel: 'Explore  '),
+                ),
               ],
-            )));
+            ),
+          ],
+        )));
   }
 }
 
-
 class MyApp extends StatelessWidget {
-
   final listOfPerson = <Person>[
-    new Person("Jeff Bezos", 177000000000, "Seattle, Washington, United States", 57),
+    new Person(
+        "Jeff Bezos", 177000000000, "Seattle, Washington, United States", 57),
     new Person("Elon Musk", 151000000000, "Austin, Texas, United States", 49),
     new Person("Bernard Arnault & family", 150000000000, "Paris, France", 72),
   ];
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
         title: 'Welcome to Flutter',
         theme: ThemeData(
@@ -156,9 +153,30 @@ class MyApp extends StatelessWidget {
             title: Text("Eat the rich"),
             leading: Icon(Icons.restaurant),
           ),
-          body: ListView(children:
-            [for (var person in listOfPerson) FoodCard(person)],
+          body: ListView(
+            children: [for (var person in listOfPerson) FoodCard(person)],
           ),
         ));
+  }
+}
+
+class PersonDetail extends StatelessWidget {
+  final Person person;
+  const PersonDetail(this.person, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(person.name)),
+      body: Container(
+          child: Center(
+        child: Column(children: [
+          Text(person.name),
+          Text(person.location),
+          Text(person.age.toString() + " years old"),
+          Text(person.money.toString() + " dollars"),
+        ]),
+      )),
+    );
   }
 }
